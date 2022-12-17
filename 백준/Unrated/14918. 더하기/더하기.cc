75e148158d9725497193801eb1e65346d9690284
Sangmin Jeon <84055441+furthermares@users.noreply.github.com>
2022-12-17 22:54:03 +0900

    [Unrated] Title: 더하기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Unrated/14918. 더하기/더하기.cc
+++ 백준/Unrated/14918. 더하기/더하기.cc
@@ -1,7 +1,8 @@
-#include <stdio.h>
+#include <iostream>
+using namespace std;
 int main() {
 	int a, b;
-	scanf("%d %d",&a,&b);
-	printf("%d\n",a+b);
+	cin >> a >> b;
+	cout << a+b << endl;
 	return 0;
 }
