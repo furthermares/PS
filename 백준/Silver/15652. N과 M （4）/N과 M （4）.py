a92025c1d186cfe9409b1d2d1efb093ee8535e87
Sangmin Jeon <84055441+furthermares@users.noreply.github.com>
2023-01-01 17:48:56 +0900

    [Silver III] Title: N과 M (4), Time: 52 ms, Memory: 30748 KB -BaekjoonHub

--- 백준/Silver/15652. N과 M （4）/N과 M （4）.py
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.py
@@ -1,16 +1,9 @@
 import sys
 def input(): return sys.stdin.readline().rstrip()
+from itertools import combinations_with_replacement
 
 n, m = map(int,input().split())
-a = []
+a = [i for i in range(1,n+1)]
 
-def f(start):
-    if len(a) < m:
-        for i in range(start,n+1):
-            a.append(i)
-            f(i)
-            a.pop()
-    else:
-        print(*a)
-
-f(1)
+for i in combinations_with_replacement(a, m):
+    print(*i)
