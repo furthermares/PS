7edba333fcda757e5e8e8c1a886f510feefc7c9e
Sangmin Jeon <84055441+furthermares@users.noreply.github.com>
2023-01-07 17:21:08 +0900

    [Silver V] Title: 부분 문자열, Time: 48 ms, Memory: 30816 KB -BaekjoonHub

---
+++ 백준/Silver/6550. 부분 문자열/부분 문자열.py
@@ -0,0 +1,20 @@
+import sys
+def input(): return sys.stdin.readline().rstrip()
+
+while True:
+    try:
+        s, t = input().split()
+    
+        idx=0
+        for i in range(len(t)):
+            if t[i] == s[idx]:
+                idx += 1
+                if idx == len(s):
+                    break
+    
+        if idx == len(s):
+            print("Yes")
+        else:
+            print("No")
+    except:
+        break
