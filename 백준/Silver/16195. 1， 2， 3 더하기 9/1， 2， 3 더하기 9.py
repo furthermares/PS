d62324a2fec06097a5a2fab463e7872647128cbd
Sangmin Jeon <84055441+furthermares@users.noreply.github.com>
2023-01-18 15:24:08 +0900

    [Silver I] Title: 1, 2, 3 더하기 9, Time: 292 ms, Memory: 48512 KB -BaekjoonHub

---
+++ 백준/Silver/16195. 1， 2， 3 더하기 9/1， 2， 3 더하기 9.py
@@ -0,0 +1,23 @@
+import sys
+def input(): return sys.stdin.readline().rstrip()
+
+MAX = 1001
+MOD = 1000000009
+
+d=[[0]*MAX for _ in range(MAX)]
+
+d[1][1] = 1
+d[2][1] = 1
+d[2][2] = 1
+d[3][1] = 1
+d[3][2] = 2
+d[3][3] = 1
+
+for i in range(4,MAX):
+    for j in range(1,i+1):
+        d[i][j] = (d[i-1][j-1] + d[i-2][j-1] + d[i-3][j-1]) % MOD
+
+T = int(input())
+for _ in range(T):
+    N, M = map(int,input().split())
+    print(sum(d[N][:M+1]) % MOD)
