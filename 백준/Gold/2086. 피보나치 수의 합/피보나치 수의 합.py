883ca560610369dbb4df21d85f36820569c56330
Sangmin Jeon <84055441+furthermares@users.noreply.github.com>
2023-01-30 14:06:19 +0900

    [Gold I] Title: 피보나치 수의 합, Time: 36 ms, Memory: 30616 KB -BaekjoonHub

---
+++ 백준/Gold/2086. 피보나치 수의 합/피보나치 수의 합.py
@@ -0,0 +1,30 @@
+import sys
+def input(): return sys.stdin.readline().rstrip()
+    
+MOD = int(1e9)
+
+M = [[1,1],[1,0]]
+def expon_by_square(a,b):
+    if b==1:
+        return a
+
+    n = expon_by_square(a, b//2)
+    if b%2:
+        return multiply_matrix(multiply_matrix(n,n),a)
+    else:
+        return multiply_matrix(n,n)
+
+def multiply_matrix(a,b):
+    c = [[0]*2 for _ in range(2)]
+    
+    for i in range(2):
+        for j in range(2):
+            for k in range(2):
+                c[i][j] += a[i][k] * b[k][j]
+            c[i][j] %= MOD
+                
+    return c
+
+a, b = map(int,input().split())
+
+print((expon_by_square(M,b+2)[0][1] - expon_by_square(M,a+1)[0][1]) % MOD)
