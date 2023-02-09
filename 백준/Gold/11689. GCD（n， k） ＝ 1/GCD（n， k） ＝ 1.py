6cd0f01d02bd9d09cc11928b5cb7e32e5d7d9e52
Sangmin Jeon <furthermares@gmail.com>
2023-02-10 07:36:52 +0900

    [Gold I] Title: GCD(n, k) = 1, Time: 168 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Gold/11689. GCD（n， k） ＝ 1/GCD（n， k） ＝ 1.py
@@ -0,0 +1,17 @@
+import sys
+def input(): return sys.stdin.readline().rstrip()
+
+def phi(n):
+	res = n
+	p = 2
+	while (p*p <= n):
+		if (n%p == 0):
+			while (n%p == 0):
+				n = int(n/p)
+			res -= int(res/p) 
+		p += 1
+	if (n>1):
+		res -= int(res/n)
+	return res
+
+print(phi(int(input())))
