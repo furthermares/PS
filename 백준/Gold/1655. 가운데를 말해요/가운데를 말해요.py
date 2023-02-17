e7e102aab9c4cb3ab3ea2dde83c25cf964cfa7df
Sangmin Jeon <furthermares@gmail.com>
2023-02-18 00:01:45 +0900

    [Gold II] Title: 가운데를 말해요, Time: 240 ms, Memory: 37268 KB -BaekjoonHub

--- 백준/Gold/1655. 가운데를 말해요/가운데를 말해요.py
+++ 백준/Gold/1655. 가운데를 말해요/가운데를 말해요.py
@@ -3,33 +3,18 @@ def input(): return sys.stdin.readline().rstrip()
 
 import heapq
 
-lst = []
+heapL, heapR = [], []
 
 for _ in range(int(input())):
-	lst.append(int(input()))
-
-heapL, heapR = [], []
-middle = lst[0]
-ans = [middle]
-
-for idx, val in enumerate(lst[1:], 1):
-	if val > middle:
-		heapq.heappush(heapR, val)
+	n = int(input())
+	
+	if len(heapL) == len(heapR):
+		heapq.heappush(heapL, -n)
 	else:
-		heapq.heappush(heapL, -val)
+		heapq.heappush(heapR, n)
 
-	if idx % 2 == 0:
-		if len(heapL) < len(heapR):
-			heapq.heappush(heapL, -middle)
-			middle = heapq.heappop(heapR)
-		elif len(heapL) > len(heapR):
-			heapq.heappush(heapR, middle)
-			middle = -heapq.heappop(heapL)
-		ans.append(middle)
-	else:
-		if len(heapL) < len(heapR):
-			ans.append(middle)
-		else:
-			ans.append(-heapL[0])
-
-print(*ans, sep="\n")
+	if heapR and heapR[0] < -heapL[0]:
+		heapq.heappush(heapL, -heapq.heappop(heapR))
+		heapq.heappush(heapR, -heapq.heappop(heapL))
+	
+	print(-heapL[0])
