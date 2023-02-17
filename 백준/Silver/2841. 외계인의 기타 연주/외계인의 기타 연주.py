b1c4b8f25e5fe61c19d3b8810c200cbdbb82b9b0
Sangmin Jeon <furthermares@gmail.com>
2023-02-17 17:42:09 +0900

    [Silver I] Title: 외계인의 기타 연주, Time: 2048 ms, Memory: 419992 KB -BaekjoonHub
    Improved.

--- 백준/Silver/2841. 외계인의 기타 연주/외계인의 기타 연주.py
+++ 백준/Silver/2841. 외계인의 기타 연주/외계인의 기타 연주.py
@@ -4,36 +4,21 @@ def input(): return sys.stdin.readline().rstrip()
 from collections import deque
 
 N, _ = map(int,input().split())
-dq = [deque() for _ in range(N)]
+dq = [deque([0]) for _ in range(N)]
 cnt = 0
 
 for _ in range(N):
 	idx, k = map(int,input().split())
 	q = dq[idx]
-	
-	if len(q) == 0:
-		q.append(k)
+
+	while q[-1] > k:
+		q.pop()
 		cnt += 1
-	else:
-		if q[-1] < k:
-			q.append(k)
-			cnt += 1
-		else:
-			while q[-1] > k:
-				if q[-1] == k:
-					break
-				else:
-					q.pop()
-					cnt += 1
-					
-					if len(q) == 0:
-						break
-			if len(q) != 0:
-				if q[-1] != k:
-					q.append(k)
-					cnt += 1
-			else:
-				q.append(k)
-				cnt += 1
+
+	if q[-1] == k:
+		continue
+
+	q.append(k)
+	cnt += 1
 
 print(cnt)
