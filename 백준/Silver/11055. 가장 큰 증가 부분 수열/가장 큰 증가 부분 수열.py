0f488be6df353baefa26f758923fde1f3f1ccf1a
Sangmin Jeon <furthermares@gmail.com>
2023-02-20 19:36:07 +0900

    [Silver II] Title: 가장 큰 증가 부분 수열, Time: 164 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/11055. 가장 큰 증가 부분 수열/가장 큰 증가 부분 수열.py
@@ -0,0 +1,13 @@
+import sys
+def input(): return sys.stdin.readline().rstrip()
+
+N = int(input())
+A = list(map(int,input().split()))
+dp = A[:]
+
+for i in range(1,N):
+    for j in range(i):
+        if A[i] > A[j]:
+            dp[i] = max(dp[i], dp[j] + A[i])
+
+print(max(dp))
