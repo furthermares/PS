83b2ca8821e0f330b855d5fb1fb949822208deb3
Sangmin Jeon <furthermares@gmail.com>
2023-02-20 22:24:53 +0900

    [Bronze IV] Title: Pizza, Time: 48 ms, Memory: 33376 KB -BaekjoonHub

---
+++ 백준/Bronze/26566. Pizza/Pizza.py
@@ -0,0 +1,15 @@
+import sys
+def input(): return sys.stdin.readline().rstrip()
+
+import math
+
+for i in range(int(input())):
+    A1, P1 = map(int,input().split())
+    R1, P2 = map(int,input().split())
+
+    R1 = R1*R1*math.pi
+
+    if A1/P1 > R1/P2:
+        print("Slice of pizza")
+    else:
+        print("Whole pizza")
