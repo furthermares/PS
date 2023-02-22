7cad1646c747ae1d0d755f405518b45261c06a78
Sangmin Jeon <furthermares@gmail.com>
2023-02-22 20:13:11 +0900

    [Gold V] Title: 숨바꼭질 3, Time: 108 ms, Memory: 35380 KB -BaekjoonHub

---
+++ 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.py
@@ -0,0 +1,25 @@
+import sys
+def input(): return sys.stdin.readline().rstrip()
+from collections import deque
+MAX = 100001
+
+N, K = map(int, input().split())
+
+visited = [0] * MAX
+def bfs():
+    q = deque([N])
+    visited[N] = 1
+    while q:
+        prev = q.popleft()
+        for next in [prev-1,prev+1,prev*2]:
+            if 0 <= next < MAX:
+                if not visited[next]:
+                    if next == prev*2:
+                        q.appendleft(next)
+                        visited[next] = visited[prev]
+                    else:
+                        q.append(next)
+                        visited[next] = visited[prev] + 1
+
+bfs()
+print(visited[K] - 1)
