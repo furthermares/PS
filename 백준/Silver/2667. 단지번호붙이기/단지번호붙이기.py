3ff3fddd6ec82647497617b06bb8055b97780b2b
Sangmin Jeon <furthermares@gmail.com>
2023-02-22 17:19:45 +0900

    [Silver I] Title: 단지번호붙이기, Time: 40 ms, Memory: 31332 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
@@ -8,7 +8,6 @@ visited = [[True if i == "0" else False for i in input()] for _ in range(N)]
 dx = [1,-1,0,0]
 dy = [0,0,1,-1]
 
-#visited = [[False] * N for _ in range(N)]
 def dfs(x,y):
     global depth
     visited[x][y] = True
