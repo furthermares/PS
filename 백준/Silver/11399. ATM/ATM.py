8f959e08825a7531a3ced4bc53d88cdb18832704
Sangmin Jeon <furthermares@gmail.com>
2023-02-23 20:26:12 +0900

    [Silver IV] Title: ATM, Time: 52 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/11399. ATM/ATM.py
@@ -0,0 +1,13 @@
+import sys
+def input(): return sys.stdin.readline().rstrip()
+
+N = int(input())
+P = list(map(int,input().split()))
+
+P.sort()
+
+ans = 0
+for i in range(N):
+    ans += P[i] * (N-i)
+
+print(ans)
