56e550ea59cdbae86e3e24591f40590ef58457c5
Sangmin Jeon <furthermares@gmail.com>
2023-02-23 17:20:32 +0900

    [Gold III] Title: 벽 부수고 이동하기, Time: 4432 ms, Memory: 189008 KB -BaekjoonHub

--- 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.py
+++ 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.py
@@ -3,66 +3,33 @@ def input(): return sys.stdin.readline().rstrip()
 from collections import deque
 
 N, M = map(int, input().split())
-graph = [[[0] * 2 for _ in range(M)] for _ in range(N)]
+
+graph = []
 for i in range(N):
-    tmp = input()
-    for j in range(M):
-        for k in range(2):
-            graph[i][j][k] = int(tmp[j])
+    graph.append(list(map(int, input())))
+
+visited = [[[0] * 2 for _ in range(M)] for _ in range(N)]
+visited[0][0][0] = 1
 
 dx, dy = [-1,1,0,0], [0,0,-1,1]
 
 def bfs():
-    q = deque([[0,0,0]])
-    graph[0][0][0] += 1
-    graph[0][0][1] += 1
+    q = deque([(0,0,0)])
     while q:
         x,y,z = q.popleft()
+
+        if x == N-1 and y == M-1:
+            return visited[x][y][z]
+        
         for i in range(4):
             nx, ny = x + dx[i], y + dy[i]
             if 0 <= nx < N and 0 <= ny < M:
-                if z == 0:
-                    if graph[nx][ny][0] == 0:
-                        q.append([nx,ny,0])
-                        graph[nx][ny][0] = graph[x][y][0] + 1
-                    if graph[nx][ny][0] == 1:
-                        q.append([nx,ny,1])
-                        graph[nx][ny][1] = graph[x][y][0] + 1
-                else:
-                    if graph[nx][ny][1] == 0:
-                        q.append([nx,ny,1])
-                        graph[nx][ny][1] = graph[x][y][1] + 1
+                if graph[nx][ny] == 1 and z == 0:
+                    q.append((nx,ny,1))
+                    visited[nx][ny][1] = visited[x][y][0] + 1
+                elif graph[nx][ny] == 0 and visited[nx][ny][z] == 0:
+                    q.append((nx, ny, z))
+                    visited[nx][ny][z] = visited[x][y][z] + 1
+    return -1
 
-bfs()
-
-N -= 1; M -= 1
-if graph[N][M][0] == 0:
-    if graph[N][M][1] == 0:
-        print(-1)
-    else:
-        print(graph[N][M][1])
-else:
-    if graph[N][M][1] == 0:
-        print(graph[N][M][0])
-    elif graph[N][M][0] < graph[N][M][1]:
-        print(graph[N][M][0])
-    else:
-        print(graph[N][M][1])
-
-"""
-print(graph)
-
-tmp = []
-for i in range(N+1):
-    for j in range(M+1):
-        tmp.append(graph[i][j][0])
-    print(*tmp)
-    tmp = []
-print()
-tmp = []
-for i in range(N+1):
-    for j in range(M+1):
-        tmp.append(graph[i][j][1])
-    print(*tmp)
-    tmp = []
-"""
+print(bfs())
