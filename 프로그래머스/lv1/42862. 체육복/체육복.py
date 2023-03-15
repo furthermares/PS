60793f13cb2321e995a515a4ce9fe9f24824caac
Sangmin Jeon <furthermares@gmail.com>
2023-03-15 14:08:45 +0900

    [level 1] Title: 체육복, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv1/42862. 체육복/체육복.py
+++ 프로그래머스/lv1/42862. 체육복/체육복.py
@@ -1,12 +1,11 @@
 def solution(n, lost, reserve):
-    u = [1] * (n+2)
-    for i in reserve:
-        u[i] += 1
-    for i in lost:
-        u[i] -= 1
-    for i in range(1,n+1):
-        if not u[i-1] and u[i] == 2:
-            u[i-1:i+1] = [1,1]
-        elif u[i] == 2 and not u[i+1]:
-            u[i:i+2] = [1,1]
-    return len([x for x in u[1:-1] if x])
+    s = set(lost) & set(reserve)
+    l = set(lost) - s
+    r = set(reserve) - s
+    
+    for i in sorted(r):
+        if i-1 in l:
+            l.remove(i-1)
+        elif i+1 in l:
+            l.remove(i+1)
+    return n - len(l)
