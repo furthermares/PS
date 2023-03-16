b340c6c10a6180443941166cd12f4497020db294
Sangmin Jeon <furthermares@gmail.com>
2023-03-16 13:40:48 +0900

    [level 2] Title: 스킬트리, Time: 0.01 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/lv2/49993. 스킬트리/스킬트리.py
+++ 프로그래머스/lv2/49993. 스킬트리/스킬트리.py
@@ -1,10 +1,10 @@
 def solution(skill, skill_trees):
     answer = 0
 
-    for skill_tree in skill_trees:
-        skill_list = list(skill)
-        for s in skill_tree:
-            if s in skill and s != skill_list.pop(0):
+    for i in skill_trees:
+        stack = list(skill)
+        for j in i:
+            if j in skill and j != stack.pop(0):
                 break
         else:
             answer += 1
