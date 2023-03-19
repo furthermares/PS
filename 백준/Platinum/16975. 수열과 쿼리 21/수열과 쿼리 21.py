b20b34243ec40caadf27bb8f60a2b017cf67b5cd
Sangmin Jeon <furthermares@gmail.com>
2023-03-19 13:20:29 +0900

    [Platinum IV] Title: 수열과 쿼리 21, Time: 964 ms, Memory: 139520 KB -BaekjoonHub

---
+++ 백준/Platinum/16975. 수열과 쿼리 21/수열과 쿼리 21.py
@@ -0,0 +1,61 @@
+import sys
+input = lambda: sys.stdin.readline().rstrip()
+import math
+
+def init(a, tree, node, start, end):
+    if start == end:
+        tree[node] = a[start]
+    else:
+        mid = (start+end)>>1
+        init(a, tree, node*2, start, mid)
+        init(a, tree, node*2+1, mid+1, end)
+        tree[node] = tree[node*2] + tree[node*2+1]
+
+def update_lazy(tree, lazy, node, start, end):
+    if lazy[node]:
+        tree[node] += (end-start+1)*lazy[node]
+        if start != end:
+            lazy[node*2] += lazy[node]
+            lazy[node*2+1] += lazy[node]
+        lazy[node] = 0
+
+def update_range(tree, lazy, node, start, end, left, right, diff):
+    update_lazy(tree, lazy, node, start, end)
+    if left > end or right < start:
+        return
+    if left <= start and end <= right:
+        tree[node] += (end-start+1)*diff
+        if start != end:
+            lazy[node*2] += diff
+            lazy[node*2+1] += diff
+        return
+    mid = (start+end)>>1
+    update_range(tree, lazy, node*2, start, mid, left, right, diff)
+    update_range(tree, lazy, node*2+1, mid+1, end, left, right, diff)
+    tree[node] = tree[node*2] + tree[node*2+1]
+
+def query(tree, lazy, node, start, end, left, right):
+    update_lazy(tree, lazy, node, start, end)
+    
+    if left > end or right < start:
+        return 0
+    if left <= start and end <= right:
+        return tree[node]
+    mid = (start+end)>>1
+    return query(tree, lazy, node*2, start, mid, left, right) + query(tree, lazy, node*2+1, mid+1, end, left, right)
+
+n = int(input())
+a = list(map(int,input().split()))
+h = math.ceil(math.log2(n))
+tree_size = 1 << (h+1)
+tree = [0] * tree_size
+lazy = [0] * tree_size
+init(a, tree, 1, 0, n-1)
+for _ in range(int(input())):
+    what, *q = map(int, input().split())
+    if what == 1:
+        left, right, diff = q
+        update_range(tree, lazy, 1, 0, n-1, left-1, right-1, diff)
+    else:
+        k = q[0]
+        print(query(tree, lazy, 1, 0, n-1, k-1, k-1))
