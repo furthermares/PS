f90ce83cc5cc2bdefd82259edd040a698253c536
Sangmin Jeon <furthermares@gmail.com>
2023-04-10 16:56:40 +0900

    Update LCA (small).py

--- Algorithms/Lowest Common Ancestor/LCA (small).py
+++ Algorithms/Lowest Common Ancestor/LCA (small).py
@@ -44,25 +44,25 @@ sys.setrecursionlimit(10**5)
 
 n = int(input())
 
-parent = [0] * (n+1) # parent node
-d = [0] * (n+1) # node depth
-visited = [False] * (n+1) # check if node is visited
-graph = [[] for _ in range(n+1)] # graph info
+parent = [0] * (n + 1) # parent node
+d = [0] * (n + 1) # node depth
+visited = [False] * (n + 1) # check if node is visited
+graph = [[] for _ in range(n + 1)] # graph info
 
-for _ in range(n-1):
+for _ in range(n - 1):
     a, b = map(int,input().split())
     graph[a].append(b)
     graph[b].append(a)
 
-# Get depth
-def get_depth(x, depth):
+# Get depths
+def dfs(x, depth):
     visited[x] = True
     d[x] = depth
     for y in graph[x]:
         if visited[y]:
             continue
         parent[y] = x
-        get_depth(y, depth + 1)
+        dfs(y, depth + 1)
 
 # Find LCA
 def lca(a, b):
@@ -72,16 +72,14 @@ def lca(a, b):
             a = parent[a]
         else:
             b = parent[b]
-    # until their nodes become the same
+    # Traverse through their ancestors until the nodes become the same
     while a != b:
         a = parent[a]
         b = parent[b]
     return a
 
-get_depth(1, 0) # Root node is 1
+dfs(1, 0) # Root node is 1
 
-m = int(input())
-
-for i in range(m):
+for i in range(int(input())):
     a, b = map(int,input().split())
     print(lca(a, b))
