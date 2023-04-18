ef311b25c436e76dc4423f940ebeceaf610e3e9c
Sangmin Jeon <furthermares@gmail.com>
2023-04-18 20:52:58 +0900

    [Gold V] Title: 선수과목 (Prerequisite), Time: 744 ms, Memory: 50464 KB -BaekjoonHub

---
+++ 백준/Gold/14567. 선수과목 （Prerequisite）/선수과목 （Prerequisite）.py
@@ -0,0 +1,30 @@
+import sys
+def input(): return sys.stdin.readline().rstrip()
+from collections import deque
+
+v, e = map(int,input().split())
+indegree = [0] * (v+1)
+graph = [[] for i in range(v+1)]
+ord = [0] * (v+1)
+
+for _ in range(e):
+    a, b = map(int,input().split())
+    graph[a].append(b)
+    indegree[b] += 1
+
+q = deque()
+
+for i in range(1, v+1):
+    if indegree[i] == 0:
+        ord[i] = 1
+        q.append(i)
+
+while q:
+    now = q.popleft()
+    for i in graph[now]:
+        indegree[i] -= 1
+        if indegree[i] == 0:
+            q.append(i)
+            ord[i] = ord[now] + 1
+
+print(*ord[1:])
