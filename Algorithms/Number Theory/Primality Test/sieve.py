ff2b2dbb3095211ba522bc40ddb222a8a1d0c3c9
Sangmin Jeon <furthermares@gmail.com>
2023-04-29 14:08:33 +0900

    Create sieve.py

---
+++ Algorithms/Number Theory/Primality Test/sieve.py
@@ -0,0 +1,12 @@
+from math import sqrt
+
+N = int(input())
+for i in range(N+1):
+    if i == 1:
+        continue
+    else:
+        for j in range(2, int(sqrt(i))+1):
+            if i%j == 0:
+                break
+        else:
+            print(i)
