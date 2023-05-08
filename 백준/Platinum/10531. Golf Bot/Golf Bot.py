fffb1cf1a50478309e6594b94eea667c1d7637d3
Sangmin Jeon <furthermares@gmail.com>
2023-05-08 20:15:11 +0900

    [Platinum I] Title: Golf Bot, Time: 576 ms, Memory: 144772 KB -BaekjoonHub

--- 백준/Platinum/10531. Golf Bot/Golf Bot.py
+++ 백준/Platinum/10531. Golf Bot/Golf Bot.py
@@ -1,10 +1,21 @@
 import sys
 def input(): return sys.stdin.readline().rstrip()
-import cmath
 
-def fft(a, inv=False):
+MOD = 998244353
+OMEGA = 3
+
+def pow(b, e):
+    r = 1
+    if e & 1:
+        r = b
+    while e:
+        e >>= 1
+        b = (b * b) % MOD
+        if e & 1: r = (r * b) % MOD
+    return r
+
+def ntt(a, inv=False):
     n = len(a)
-    w = [cmath.rect(1, (-2 if inv else 2) * cmath.pi * i / n) for i in range(n >> 1)]
     rev = [0] * n
     for i in range(n):
         rev[i] = rev[i >> 1] >> 1
@@ -13,42 +24,49 @@ def fft(a, inv=False):
         if i < rev[i]:
             a[i], a[rev[i]] = a[rev[i]], a[i]
 
+    ang = pow(OMEGA, (MOD - 1) // n)
+    if inv:
+        ang = pow(ang, MOD - 2);
+    w = [0] * (n >> 1)
+    w[0] = 1
+    for i in range(1, n >> 1):
+        w[i] = w[i-1] * ang % MOD
+    
     step = 2
     while step <= n:
         half, diff = step >> 1, n // step
         for i in range(0, n, step):
             pw = 0
             for j in range(i, i + half):
-                v = a[j + half] * w[pw]
-                a[j + half] = a[j] - v
-                a[j] += v
-                pw += diff
+                v = a[j + half] * w[pw] % MOD
+                a[j + half] = (a[j] - v) % MOD
+                a[j] = (a[j] + v) % MOD
+                pw = (pw + diff) % MOD
         step <<= 1
 
+    t = pow(n, MOD - 2)
     if inv:
         for i in range(n):
-            a[i] /= n
+            a[i] = a[i] * t % MOD
 
-def fft_conv(a, b):
+def ntt_conv(a, b):
     s = len(a) + len(b) - 1
     n = 1 << s.bit_length()
-    a.extend([0.0] * (n - len(a)))
-    b.extend([0.0] * (n - len(b)))
+    a.extend([0] * (n - len(a)))
+    b.extend([0] * (n - len(b)))
 
-    fft(a), fft(b)
+    ntt(a), ntt(b)
     for i in range(n):
-        a[i] *= b[i]
-    fft(a, True)
-
-    a = [round(a[i].real) for i in range(s)]
+        a[i] = a[i] * b[i]
+    ntt(a, True)
     return a
-
+    
 f = [0] * 200001
 for _ in range(int(input())):
     f[int(input())] = 1
 f[0] = 1
 
-res = fft_conv(f,f[:])
+res = ntt_conv(f,f[:])
 
 cnt = 0
 for _ in range(int(input())):
