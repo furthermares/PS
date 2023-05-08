733943d16700b3138a05ddec95fab7b07e6b3fb6
Sangmin Jeon <furthermares@gmail.com>
2023-05-08 20:13:42 +0900

    [Platinum I] Title: 이동, Time: 348 ms, Memory: 140920 KB -BaekjoonHub

--- 백준/Platinum/1067. 이동/이동.py
+++ 백준/Platinum/1067. 이동/이동.py
@@ -1,29 +1,66 @@
 import sys
 def input(): return sys.stdin.readline().rstrip()
-from cmath import exp, pi
 
-def fft(x, inv=False):
-    n = len(x)
-    if n == 1: return x
-    even = fft(x[0::2], inv)
-    odd = fft(x[1::2], inv)
-    if not inv:
-        T = [exp(-2j*pi*k/n)*odd[k] for k in range(n>>1)]
-    else:
-        T = [exp(2j*pi*k/n)*odd[k] for k in range(n>>1)]
-    return [even[k] + T[k] for k in range(n>>1)] + [even[k] - T[k] for k in range(n>>1)]
+MOD = 998244353
+OMEGA = 3
+
+def pow(b, e):
+    r = 1
+    if e & 1:
+        r = b
+    while e:
+        e >>= 1
+        b = (b * b) % MOD
+        if e & 1: r = (r * b) % MOD
+    return r
+
+def ntt(a, inv=False):
+    n = len(a)
+    rev = [0] * n
+    for i in range(n):
+        rev[i] = rev[i >> 1] >> 1
+        if i & 1:
+            rev[i] |= n >> 1
+        if i < rev[i]:
+            a[i], a[rev[i]] = a[rev[i]], a[i]
+
+    ang = pow(OMEGA, (MOD - 1) // n)
+    if inv:
+        ang = pow(ang, MOD - 2);
+    w = [0] * (n >> 1)
+    w[0] = 1
+    for i in range(1, n >> 1):
+        w[i] = w[i-1] * ang % MOD
     
-def conv(A,B):
-    n = 1
-    while n <= max(len(X),len(Y)):
-        n <<= 1
-    A += [0]*(n - len(A))
-    B += [0]*(n - len(B))
-    AT = fft(A)
-    BT = fft(B)
-    C = [AT[i]*BT[i] for i in range(n)]
-    return [round((a/n).real) for a in fft(C, True)]
+    step = 2
+    while step <= n:
+        half, diff = step >> 1, n // step
+        for i in range(0, n, step):
+            pw = 0
+            for j in range(i, i + half):
+                v = a[j + half] * w[pw] % MOD
+                a[j + half] = (a[j] - v) % MOD
+                a[j] = (a[j] + v) % MOD
+                pw = (pw + diff) % MOD
+        step <<= 1
 
+    t = pow(n, MOD - 2)
+    if inv:
+        for i in range(n):
+            a[i] = a[i] * t % MOD
+
+def ntt_conv(a, b):
+    s = len(a) + len(b) - 1
+    n = 1 << s.bit_length()
+    a.extend([0] * (n - len(a)))
+    b.extend([0] * (n - len(b)))
+
+    ntt(a), ntt(b)
+    for i in range(n):
+        a[i] = a[i] * b[i]
+    ntt(a, True)
+    return a
+    
 input()
 X = list(map(int,input().split()))
 Y = list(map(int,input().split()))
@@ -31,4 +68,4 @@ Y = list(map(int,input().split()))
 X = X+X
 Y = Y[::-1]
 
-print(max(conv(X,Y)))
+print(max(ntt_conv(X,Y)))
