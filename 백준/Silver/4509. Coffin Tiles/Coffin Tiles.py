745d82432f596ed4021857c1fe9d0e49f9d9ce96
Sangmin Jeon <furthermares@gmail.com>
2023-06-12 12:33:09 +0900

    [Silver IV] Title: Coffin Tiles, Time: 160 ms, Memory: 122320 KB -BaekjoonHub

--- 백준/Silver/4509. Coffin Tiles/Coffin Tiles.py
+++ 백준/Silver/4509. Coffin Tiles/Coffin Tiles.py
@@ -1,25 +1,9 @@
-import sys
-input = sys.stdin.readline
-inm = lambda: map(int,input().split())
-
-MAX_SQ = 1000000
-MAX_N = 1001
-num_factors = [0] * (MAX_SQ+1)
-ans = [0] * MAX_N
-
-p = 1
-while p * p <= MAX_SQ:
-    for i in range(p * p, MAX_SQ+1, p):
-        num_factors[i] += 1
-    p += 1
-
-for i in range(MAX_SQ + 1):
-    if not ans[num_factors[i]]:
-        ans[num_factors[i]] = i
-
+M=1001;S=M*M;f=[0]*S;a=[0]*M
+p=1
+while p*p<S:
+ for i in range(p*p,S,p):f[i]+=1
+ p+=1
+for i in range(S):
+ if a[f[i]]==0:a[f[i]]=i
 input()
-for n in inm():
-    if n < MAX_N and ans[n]:
-        print(ans[n])
-    else:
-        print("Too big")
+for n in map(int,input().split()):print(a[n])if(n<M)*a[n]else print("Too big")
