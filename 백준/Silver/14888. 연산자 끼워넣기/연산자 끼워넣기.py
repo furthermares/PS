5f1cb28c65e387a61d84ce21df5b7eddd91d0b9f
Sangmin Jeon <furthermares@gmail.com>
2023-06-15 11:45:19 +0900

    [Silver I] Title: 연산자 끼워넣기, Time: 268 ms, Memory: 119888 KB -BaekjoonHub

--- 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.py
+++ 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.py
@@ -5,27 +5,27 @@ N = int(input())
 A = list(map(int,input().split()))
 add, sub, mul, div = map(int,input().split())
 
-max_ans, min_ans = -1e9, 1e9
+max_ans, min_ans = -sys.maxsize, sys.maxsize
 
 def dfs(turn, ans_cur):
     global add, sub, mul, div, max_ans, min_ans
     if turn == N-1:
-        max_ans=max(max_ans,ans_cur)
-        min_ans=min(min_ans,ans_cur)
+        max_ans = max(max_ans, ans_cur)
+        min_ans = min(min_ans, ans_cur)
     else:
-        if add > 0:
+        if add:
             add-=1
             dfs(turn+1, ans_cur + A[turn+1])
             add+=1
-        if sub > 0:
+        if sub:
             sub-=1
             dfs(turn+1, ans_cur - A[turn+1])
             sub+=1
-        if mul > 0:
+        if mul:
             mul-=1
             dfs(turn+1, ans_cur * A[turn+1])
             mul+=1
-        if div > 0:
+        if div:
             div-=1
             dfs(turn+1, int(ans_cur / A[turn+1]))
             div+=1
