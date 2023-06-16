02bed994f32ca7e8392ec4122a73c2e7f8898def
Sangmin Jeon <furthermares@gmail.com>
2023-06-16 18:06:29 +0900

    [Platinum III] Title: Censoring, Time: 4364 ms, Memory: 237792 KB -BaekjoonHub

---
+++ 백준/Platinum/10747. Censoring/Censoring.py
@@ -0,0 +1,47 @@
+input = __import__('sys').stdin.readline
+ins = lambda: input().rstrip()
+
+def fail(p):
+    n = len(p)
+    lps = [0] * n
+    j = 0
+    for i in range(1, n):
+        while j and p[i] != p[j]:
+            j = lps[j - 1]
+        if p[i] == p[j]:
+            j += 1
+            lps[i] = j
+    return lps
+
+def KMP(t, p):
+    lt, lp = len(t), len(p)
+
+    lps = fail(p)
+    
+    i = j = 0
+    j_his = [0]
+    while i < lt:
+        if t[i] == p[j]:
+            i += 1
+            j += 1
+            j_his.append(j)
+        else:
+            if j:
+                j = lps[j-1]
+            else:
+                i += 1
+                j_his.append(j)
+        
+        if j == lp:
+            t = t[:i - lp] + t[i:]
+            j_his = j_his[:-lp]
+            j = j_his[-1]
+            lt -= lp
+            i -= lp
+
+    print(t)
+    
+S = ins()
+T = ins()
+
+KMP(S, T)
