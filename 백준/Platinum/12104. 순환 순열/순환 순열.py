e9ecbd816a192fb62e50772c38866b09359a973e
Sangmin Jeon <furthermares@gmail.com>
2023-06-25 15:43:37 +0900

    [Platinum V] Title: 순환 순열, Time: 144 ms, Memory: 122324 KB -BaekjoonHub

---
+++ 백준/Platinum/12104. 순환 순열/순환 순열.py
@@ -0,0 +1,43 @@
+input = __import__('sys').stdin.readline
+ins = lambda: input().rstrip()
+
+def fail(p):
+    n = len(p)
+    lps = [0] * n
+    j = 0
+    for i in range(1, n):
+        while j and p[i] != p[j]:
+            j = lps[j - 1]
+        if p[i] == p[j]:
+            j += 1
+            lps[i] = j
+    return lps
+
+def KMP(t, p):
+    lt, lp = len(t), len(p)
+    res = []
+
+    lps = fail(p)
+
+    i = j = 0
+    while i < lt:
+        if t[i] == p[j]:
+            i += 1
+            j += 1
+        else:
+            if j:
+                j = lps[j-1]
+            else:
+                i += 1
+        
+        if j == lp:
+            res.append(i-j+1)
+            j = lps[j-1]
+
+    return res
+
+A = ins()
+B = ins()
+
+res = KMP((A*2)[:-1], B)
+print(len(res))
