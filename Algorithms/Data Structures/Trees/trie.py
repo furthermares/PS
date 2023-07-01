998bcff7d1fbf552d76fab8aca2972b9393aa7d1
Sangmin Jeon <furthermares@gmail.com>
2023-07-01 19:07:06 +0900

    Implemented init+input, __repr__

--- Algorithms/Data Structures/Trees/trie.py
+++ Algorithms/Data Structures/Trees/trie.py
@@ -1,7 +1,9 @@
 class TrieNode:
-    def __init__(self):
+    def __init__(self, *words):
         self.nodes: dict[str, TrieNode] = {} # Mapping from char to TrieNode
         self.is_leaf = False
+        for word in words:
+            self.insert(word)
 
     def insert(curr, word):
         for char in word:
@@ -10,7 +12,7 @@ class TrieNode:
             curr = curr.nodes[char]
         curr.is_leaf = True
         
-    def find(curr, word) -> bool:
+    def find(curr, word):
         for char in word:
             if char not in curr.nodes:
                 return False
@@ -20,37 +22,31 @@ class TrieNode:
     def delete(self, word):
         def _delete(curr, word, idx):
             if idx == len(word):
-                # If word does not exist
                 if not curr.is_leaf:
                     return False
                 curr.is_leaf = False
                 return len(curr.nodes) == 0
             char = word[idx]
             char_node = curr.nodes.get(char)
-            # If char not in current trie node
             if not char_node:
                 return False
-            # Flag to check if node can be deleted
             delete_curr = _delete(char_node, word, idx + 1)
             if delete_curr:
                 del curr.nodes[char]
                 return len(curr.nodes) == 0
             return delete_curr
-
         _delete(self, word, 0)
 
-    def print(self, word = ""):
+    def __repr__(self, word = ""):
+        words = []
         def _print_words(node, word):
             if node.is_leaf:
-                print(word, end=" ")
-        
+                words.append(word)
             for key, value in node.nodes.items():
                 _print_words(value, word + key)
-
         _print_words(self, "")
+        return " ".join(words)
 
 words = "banana bananas bandana band apple all beast".split()
-root = TrieNode()
-for word in words:
-    root.insert(word)
-root.print()
+root = TrieNode(words)
+print(root)
