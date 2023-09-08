1ef530b89dd985c57f5b5f2d181380d59cfedb44
Sangmin Jeon <furthermares@gmail.com>
2023-09-08 16:32:47 +0900

    Update Maximum flow - Edmonds-Karp.py

--- Algorithms/Graphs/Flows and related problems/Maximum flow - Edmonds-Karp.py
+++ Algorithms/Graphs/Flows and related problems/Maximum flow - Edmonds-Karp.py
@@ -73,11 +73,11 @@ def FordFulkerson(S, T):
     return max_flow
 
 G = [[0, 16, 13, 0, 0, 0],
-		[0, 0, 10, 12, 0, 0],
-		[0, 4, 0, 0, 14, 0],
-		[0, 0, 9, 0, 0, 20],
-		[0, 0, 0, 7, 0, 4],
-		[0, 0, 0, 0, 0, 0]]
+	[0, 0, 10, 12, 0, 0],
+	[0, 4, 0, 0, 14, 0],
+	[0, 0, 9, 0, 0, 20],
+	[0, 0, 0, 7, 0, 4],
+	[0, 0, 0, 0, 0, 0]]
 source = 0; sink = 5
 
 print("The maximum possible flow is %d " % FordFulkerson(source, sink))
