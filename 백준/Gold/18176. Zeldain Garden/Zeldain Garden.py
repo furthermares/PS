f22dc1b50118cbdb5bd79e476b4e11bfdb90a8d4
Sangmin Jeon <furthermares@gmail.com>
2023-12-04 13:19:15 +0900

    [Gold IV] Title: Zeldain Garden, Time: 228 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Gold/18176. Zeldain Garden/Zeldain Garden.py
+++ 백준/Gold/18176. Zeldain Garden/Zeldain Garden.py
@@ -1,3 +1,3 @@
-def f(N):q=int(N**0.5);return sum(N//i for i in range(1,q+1))*2-q*q
+def f(N):q=int(N**.5);return sum(N//i for i in range(1,q+1))*2-q*q
 N,M=map(int,input().split())
 print(f(M)-f(N-1))
