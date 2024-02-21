3a16fff2ec0a880670e2194e1c86d73963a7eea8
Sangmin Jeon <furthermares@gmail.com>
2024-02-21 10:08:04 +0900

    [Silver III] Title: Задача, Time: 140 ms, Memory: 42036 KB -BaekjoonHub

--- 백준/Silver/29106. Задача/Задача.py
+++ 백준/Silver/29106. Задача/Задача.py
@@ -1,15 +1,8 @@
-input=__import__('sys').stdin.readline
-inp=lambda:int(input())
-inm=lambda:map(int,input().split())
-inl=lambda:[*map(int,input().split())]
-
-N,K=inm()
-A=inl()
-A.sort()
-l=0
-a=l
+I=lambda:map(int,input().split())
+N,K=I()
+A=sorted(I())
+l=x=0
 while l<N-K+1:
-    if A[l+K-1]-A[l]<A[a+K-1]-A[a]:
-        a=l
-    l+=1
-print(*A[a:a+K])
+ if A[l+K-1]-A[l]<A[x+K-1]-A[x]:x=l
+ l+=1
+print(*A[x:x+K])
