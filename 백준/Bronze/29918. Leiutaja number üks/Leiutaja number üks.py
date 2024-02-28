21ea21999cbd3e7f291a9ec3c04c09a6e85613e2
Sangmin Jeon <furthermares@gmail.com>
2024-02-28 22:03:14 +0900

    [Bronze III] Title: Leiutaja number üks, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/29918. Leiutaja number üks/Leiutaja number üks.py
+++ 백준/Bronze/29918. Leiutaja number üks/Leiutaja number üks.py
@@ -1,4 +1,4 @@
 N,M=map(int,input().split())
 b=0
-for _ in range(5):n,m=map(int,input().split());    b=max(b,n+m*10)
+for _ in[0]*5:n,m=map(int,input().split());b=max(b,n+m*10)
 print(max(0,b-N-M*10+1))
