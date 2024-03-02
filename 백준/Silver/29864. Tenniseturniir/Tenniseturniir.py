9d5f81edfd08aee759cddea1df4fca056902c6fd
Sangmin Jeon <furthermares@gmail.com>
2024-03-02 15:40:50 +0900

    [Silver V] Title: Tenniseturniir, Time: 60 ms, Memory: 34016 KB -BaekjoonHub

--- 백준/Silver/29864. Tenniseturniir/Tenniseturniir.py
+++ 백준/Silver/29864. Tenniseturniir/Tenniseturniir.py
@@ -1,6 +1,6 @@
-I=lambda:map(int,input().split())
 from collections import*
-N,A,c,P=I(),Counter(I()),[[],[],[]],print
+I,P=lambda:map(int,input().split()),print
+N,A,c=I(),Counter(I()),[[],[],[]]
 for n in N:
  for i in range(1,A[n]+1):c[i-1].append(n)
 P(*c[2]);P(*c[1]);P(*c[0])
