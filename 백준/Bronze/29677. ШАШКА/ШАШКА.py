4327c85cc71cd412ee8248eb1da229e19097d1d6
Sangmin Jeon <furthermares@gmail.com>
2024-03-04 13:40:38 +0900

    [Bronze I] Title: ШАШКА, Time: 128 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/29677. ШАШКА/ШАШКА.py
+++ 백준/Bronze/29677. ШАШКА/ШАШКА.py
@@ -1,11 +1,5 @@
 I,i=input,int
 R={}
 for _ in[0]*i(I()):r,P=I().split();R[P]=i(r)
-for _ in[0]*i(I()):
- A,B,X=I().split()
- S=.5if X=="0"else 1if X=="1"else 0
- EA=1+10**((R[B]-R[A])/400)
- EB=1+10**((R[A]-R[B])/400)
- R[A]=max(i(R[A]+15*(S-1/EA)),0)
- R[B]=max(i(R[B]+15*(1-S-1/EB)),0)
+for _ in[0]*i(I()):A,B,X=I().split();S=.5if X=="0"else 1if X=="1"else 0;E=1+10**((R[B]-R[A])/400);e=1+10**((R[A]-R[B])/400);R[A]=max(i(R[A]+15*(S-1/E)),0);R[B]=max(i(R[B]+15*(1-S-1/e)),0)
 for P,r in sorted(R.items(),key=lambda x:(-x[1],x[0])):print(r,P)
