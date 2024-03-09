2369aa3cb73b19ffd49c4b81be6da108f3622a57
Sangmin Jeon <furthermares@gmail.com>
2024-03-09 14:50:42 +0900

    [Bronze II] Title: Прямоугольный полигон, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/29644. Прямоугольный полигон/Прямоугольный полигон.py
+++ 백준/Bronze/29644. Прямоугольный полигон/Прямоугольный полигон.py
@@ -1,8 +1,6 @@
 S,P=map(int,input().split())
 if P%2==0:
-    for A in range(1,30001):
-        B=P//2-A
-        if A*B==S:
-            print(B,A)
-            exit()
+ for A in range(15001):
+  B=P//2-A
+  if A*B==S:print(B,A);exit()
 print(-1)
