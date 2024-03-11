fe0b633d3f67e8bf8a5f9892658b710a0cc858c9
Sangmin Jeon <furthermares@gmail.com>
2024-03-11 10:38:18 +0900

    [Bronze II] Title: Расписание, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/29602. Расписание/Расписание.py
+++ 백준/Bronze/29602. Расписание/Расписание.py
@@ -1,6 +1,6 @@
 N=int(input())
 T=[*map(int,input().split())]
-for i in range(N):T[i]=[T[i],i+1]
+for i in range(N):T[i]=[T[i],i]
 T.sort()
 for i in range(N):T[i]+=[i+1]
-print(*[t[2] for t in sorted(T,key=lambda x:x[1])])
+print(*[t[2]for t in sorted(T,key=lambda x:x[1])])
