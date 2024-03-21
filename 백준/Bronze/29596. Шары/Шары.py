5efe832f5ac375da4690b203b06985b2019f170b
Sangmin Jeon <furthermares@gmail.com>
2024-03-21 12:39:35 +0900

    [Bronze I] Title: Шары, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/29596. Шары/Шары.py
+++ 백준/Bronze/29596. Шары/Шары.py
@@ -1,7 +1,7 @@
-I=lambda:map(int,input().split())
+I,P=lambda:map(int,input().split()),print
 X,V=I()
 x,v=I()
 T=int(input())
 D,d=X+V*T,x+v*T
-if (X-x)*(D-d)>0:print(D,V);print(d,v)
-else:print(d,v);print(D,V)
+if (X-x)*(D-d)>0:P(D,V);P(d,v)
+else:P(d,v);P(D,V)
