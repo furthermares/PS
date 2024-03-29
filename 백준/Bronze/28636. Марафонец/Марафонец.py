35d05b18694747e80418ed02714b87ac2cef661b
Sangmin Jeon <furthermares@gmail.com>
2024-03-29 11:21:27 +0900

    [Bronze III] Title: Марафонец, Time: 84 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/28636. Марафонец/Марафонец.py
+++ 백준/Bronze/28636. Марафонец/Марафонец.py
@@ -1,2 +1,2 @@
-t=sum(m*60+s for m,s in[map(int,input().split(":"))for _ in[0]*int(input())])
+t=sum(s+m*60for m,s in[map(int,input().split(":"))for _ in[0]*int(input())])
 print(f"{t//3600:02d}:{t%3600//60:02d}:{t%60:02d}")
