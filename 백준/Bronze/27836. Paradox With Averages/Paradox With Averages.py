2850cdc1b5505709af0be82a191b6740892732cf
Sangmin Jeon <furthermares@gmail.com>
2024-04-07 11:09:39 +0900

    [Bronze I] Title: Paradox With Averages, Time: 180 ms, Memory: 54208 KB, Score: 3 point -BaekjoonHub

--- 백준/Bronze/27836. Paradox With Averages/Paradox With Averages.py
+++ 백준/Bronze/27836. Paradox With Averages/Paradox With Averages.py
@@ -1,3 +1,3 @@
 i=input
 I=lambda:map(int,i().split())
-for _ in[0]*int(i()):i();N,n=I();C=[*I()];E=[*I()];print(sum(sum(E)/n<c<sum(C)/N for c in C))
+for _ in[0]*int(i()):i();N,n=I();C=[*I()];c=sum(C)/N;e=sum(I())/n;print(sum(e<j<c for j in C))
