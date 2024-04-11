a590c026eed9b608f153c577ac7b87b616466820
Sangmin Jeon <furthermares@gmail.com>
2024-04-11 11:56:08 +0900

    [Bronze I] Title: Queue skipping (Hard), Time: 2616 ms, Memory: 216812 KB -BaekjoonHub

---
+++ 백준/Bronze/27660. Queue skipping （Hard）/Queue skipping （Hard）.py
@@ -0,0 +1,7 @@
+I=input
+for _ in[0]*int(I()):
+ I();N,E=map(int,I().split());e=[int(I())for _ in[0]*E][::-1];n=set(range(1,N+1))
+ for i in range(E):
+  if len(n)==1:break
+  if e[i]in n:n.remove(e[i])
+ print(max(n))
