ca1e4dccc0720074081e6f4778eb55af77f4e2f1
Sangmin Jeon <furthermares@gmail.com>
2024-05-15 14:37:48 +0900

    [Platinum I] Title: 완전 중요한 간선, Time: 1704 ms, Memory: 34136 KB -BaekjoonHub

--- 백준/Platinum/5651. 완전 중요한 간선/완전 중요한 간선.py
+++ 백준/Platinum/5651. 완전 중요한 간선/완전 중요한 간선.py
@@ -7,63 +7,63 @@ from collections import deque
 def solve():
     def edmonds_karp():
         while True:
-            q = deque([s])
             parent = [-1]*(N+1)
+            q = deque([s])
             while q:
                 cur = q.popleft()
                 for nxt in E[cur]:
-                    if parent[nxt] == -1 and C[cur][nxt] > 0:
-                        q.append(nxt)
+                    if C[cur][nxt] - F[cur][nxt] > 0 and parent[nxt] == -1:
                         parent[nxt] = cur
-                        if nxt == e: break   
-
+                        if nxt == e: break
+                        q.append(nxt)  
+    
             if parent[e] == -1: break
-
-            nxt = e
+    
+            cur = e
             maxflow = float("Inf")
-            while nxt != s:
-                maxflow = min(maxflow, C[parent[nxt]][nxt])
-                nxt = parent[nxt]
-
-            nxt = e
-            while nxt != s:
-                C[parent[nxt]][nxt] -= maxflow
-                C[nxt][parent[nxt]] += maxflow
-                nxt = parent[nxt]
-
+            while cur != s:
+                maxflow = min(maxflow, C[parent[cur]][cur] - F[parent[cur]][cur])
+                cur = parent[cur]
+    
+            cur = e
+            while cur != s:
+                F[parent[cur]][cur] += maxflow
+                F[cur][parent[cur]] -= maxflow
+                cur = parent[cur]
+    
     for _ in range(inp()):
         N,M = inm()
-
+    
         E = [[] for _ in range(N+1)]
         Es = []
         C = [[0]*(N+1) for _ in range(N+1)]
+        F = [[0]*(N+1) for _ in range(N+1)]
         s,e = 1,N
-
+    
         for _ in range(M):
             f,t,b = inm()
             E[f].append(t)
             E[t].append(f)
             Es.append((f,t))
             C[f][t] += b
-
+    
         edmonds_karp()
-
+    
         ans = 0
         for s, e in Es:
-            if C[s][e] == 0:
+            if C[s][e] - F[s][e] == 0:
                 parent = [-1]*(N+1)
-    
                 q = deque([s])
                 while q:
                     cur = q.popleft()
                     for nxt in E[cur]:
-                        if parent[nxt] == -1 and C[cur][nxt] > 0:
+                        if C[cur][nxt] - F[cur][nxt] > 0 and parent[nxt] == -1:
                             parent[nxt] = cur
-                            q.append(nxt)
                             if nxt == e: break
-    
+                            q.append(nxt)
+        
                 ans += parent[e] == -1
-
+    
         print(ans)
-
+    
 if __name__ == "__main__": solve()
