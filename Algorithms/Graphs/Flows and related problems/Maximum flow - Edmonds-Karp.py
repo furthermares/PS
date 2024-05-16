b4d01f25a9c676be6ab23a746802a6306e88494a
Sangmin Jeon <furthermares@gmail.com>
2024-05-16 12:08:39 +0900

    overhaul

--- Algorithms/Graphs/Flows and related problems/Maximum flow - Edmonds-Karp.py
+++ Algorithms/Graphs/Flows and related problems/Maximum flow - Edmonds-Karp.py
@@ -1,80 +1,58 @@
-# https://www.geeksforgeeks.org/ford-fulkerson-algorithm-for-maximum-flow-problem/
+from collections import deque
 
-# uses adjacency matrix representation
-
-# Returns the maximum flow from s to t in the given graph
-def edmonds_karp(S, T):
+def edmonds_karp(s,e):
+    max_flow = 0
     
-    # This array is filled by BFS and to store path
-    parent = [-1] * len(G)
-    max_flow = 0 # There is no flow initially
+    while True:
+        parent = [-1]*(N+1)
+        q = deque([s])
+        while q:
+            cur = q.popleft()
+            for nxt in E[cur]:
+                if C[cur][nxt] - F[cur][nxt] > 0 and parent[nxt] == -1:
+                    parent[nxt] = cur
+                    if nxt == e: break
+                    q.append(nxt)  
+        if parent[e] == -1: break
 
-	# Returns true if there is a path from source 's' to sink 't' in residual graph.
-	# Also fills parent[] to store the path
-	def BFS(S, T, parent):
-
-    # Mark all the vertices as not visited
-    visited = [False] * len(G)
-
-    # Create a queue for BFS
-    q = [S]
-
-    # Mark the source node as visited and enqueue it
-    visited[S] = True
-
-    # Standard BFS Loop
-    while q:
-
-        # Dequeue a vertex from q and print it
-        u = q.pop(0)
-
-        # Get all adjacent vertices of the dequeued vertex u.
-        # If an adjacent has not been visited, then mark it visited and enqueue it.
-        for idx, val in enumerate(G[u]):
-            if not visited[idx] and val > 0:
-                # If we find a connection to the sink node,
-                # then there is no point in BFS anymore.
-                # We just have to set its parent and canreturn True.
-                q.append(idx)
-                visited[idx] = True
-                parent[idx] = u
-                if idx == T:
-                    return True
-
-    # We didn't reach sink in BFS starting from source, so return False.
-    return False
-	
-    # Augment the flow while there is path from S to T
-    while BFS(S, T, parent):
-
-        # Find minimum residual capacity of the edges along the path filled by BFS.
-        # Or we can say find the maximum flow through the path found.
         path_flow = float("Inf")
-        
-        s = T
-        while(s != S):
-            path_flow = min(path_flow, G[parent[s]][s])
-            s = parent[s]
-
-        # Add path flow to overall flow
+        i = e
+        while i != s:
+            path_flow = min(path_flow, C[parent[i]][i] - F[parent[i]][i])
+            i = parent[i]
         max_flow += path_flow
 
-        # update residual capacities of the edges and reverse edges along the path
-        v = T
-        while(v != S):
-            u = parent[v]
-            G[u][v] -= path_flow
-            G[v][u] += path_flow
-            v = parent[v]
+        i = e
+        while i != s:
+            F[parent[i]][i] += path_flow
+            F[i][parent[i]] -= path_flow
+            i = parent[i]
 
     return max_flow
 
-G = [[0, 16, 13, 0, 0, 0],
-	[0, 0, 10, 12, 0, 0],
-	[0, 4, 0, 0, 14, 0],
-	[0, 0, 9, 0, 0, 20],
-	[0, 0, 0, 7, 0, 4],
-	[0, 0, 0, 0, 0, 0]]
-source = 0; sink = 5
+N,M = inm()				# # of nodes, # of edges
 
-print("The maximum possible flow is %d " % edmonds_karp(source, sink))
+E = [[] for _ in range(N+1)] 		# Edges
+C = [[0]*(N+1) for _ in range(N+1)]	# Capacity
+F = [[0]*(N+1) for _ in range(N+1)]	# Flow
+s,e = 1,2				# source, sink
+
+for _ in range(M):
+    f,t,b = inm()			# from, to, value
+    f,t = inm()
+    E[f].append(t)
+    E[t].append(f)
+    C[f][t] += b
+
+print(edmonds_karp(s,e))
+
+"""
+# Input
+5 5 (# of Node, # of Edges)
+1 3 1 (from, to, value)
+3 2 1
+1 5 1
+5 4 1
+4 2 1
+
+"""
