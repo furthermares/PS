13f237d103df560a9a23fbd21de209d446a82133
Sangmin Jeon <furthermares@gmail.com>
2024-05-17 21:22:30 +0900

    [Platinum II] Title: 격자 0 만들기, Time: 15660 ms, Memory: 482268 KB -BaekjoonHub

--- 백준/Platinum/11495. 격자 0 만들기/격자 0 만들기.py
+++ 백준/Platinum/11495. 격자 0 만들기/격자 0 만들기.py
@@ -5,18 +5,18 @@ inl=lambda:[*map(int,input().split())]
 
 from collections import deque
 
-def add_edge(u,v,c,E,C):
+def add_edge(u,v,c=1):
     E[u].append(v)
     E[v].append(u)
     C[u][v] = c
 
-def edmonds_karp(s,e,N,M,E,C,F,G):
+def edmonds_karp(s,e):
     max_flow = 0
 
     while True:
         parent = [-1]*(N*M+2)
         q = deque([s])
-        while q:
+        while q and parent[e] == -1:
             cur = q.popleft()
             for nxt in E[cur]:
                 if C[cur][nxt] - F[cur][nxt] > 0 and parent[nxt] == -1:
@@ -43,35 +43,25 @@ def edmonds_karp(s,e,N,M,E,C,F,G):
 dx = (1,-1,0,0)
 dy = (0,0,1,-1)
 
-def solve():
+for _ in range(inp()):
     N,M = inm()
 
     E = [[] for _ in range(N*M+2)]
     C = [[0]*(N*M+2) for _ in range(N*M+2)]
     F = [[0]*(N*M+2) for _ in range(N*M+2)]
     s,e = N*M,N*M+1
-    G = []
-    t=0
-    for _ in range(N):
-        tt=inl()
-        G.append(tt)
-        t += sum(tt)
-    #G = [inl() for _ in range(N)]
+    G = [inl() for _ in range(N)]
 
     for x in range(N):
         for y in range(M):
             if (x+y)%2:
-                add_edge(s,x*M+y,G[x][y],E,C)
+                add_edge(s,x*M+y,G[x][y])
 
                 for i in range(4):
                     nx,ny = x+dx[i],y+dy[i]
                     if 0<=nx<N and 0<=ny<M:
-                        add_edge(x*M+y, nx*M+ny, float("inf"),E,C)
+                        add_edge(x*M+y, nx*M+ny, float("inf"))
             else:
-                add_edge(x*M+y,e,G[x][y],E,C)
+                add_edge(x*M+y,e,G[x][y])
 
-    print(t - edmonds_karp(s,e,N,M,E,C,F,G))
-
-if __name__ == "__main__":
-    for _ in range(inp()):
-        solve()
+    print(sum(map(sum,G)) - edmonds_karp(s,e))
